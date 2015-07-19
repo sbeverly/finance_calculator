b84e03a9a1d13cf4f2a7aec3071d7a801165b01e
Siyan Beverly <siyan.beverly@gmail.com>
2015-07-19 14:44:35 -0400

    add pull history function

--- investmentCalculator.js
+++ investmentCalculator.js
@@ -1,2 +1,11 @@
 var yahoo = require('yahoo-finance');
-var exports = module.exports = {}
+var exports = module.exports = {};
+
+exports.pullHistory = function(symbol, dates, callback) {
+	yahoo.historical({
+	  symbol: symbol,
+	  from: dates.from,
+	  to: dates.to,
+	  // period: 'd'  // 'd' (daily), 'w' (weekly), 'm' (monthly), 'v' (dividends only)
+	}, callback)
+}
