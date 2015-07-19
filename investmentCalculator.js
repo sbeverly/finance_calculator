cbd7ef112af4fede02bb6fd0ad8288f0cffc8194
Siyan Beverly <siyan.beverly@gmail.com>
2015-07-19 15:53:16 -0400

    add function to find inflection points in historical prices

--- investmentCalculator.js
+++ investmentCalculator.js
@@ -1,5 +1,6 @@
 var yahoo = require('yahoo-finance');
 var exports = module.exports = {};
+var _ = require('underscore');
 
 exports.pullHistory = function(symbol, dates, callback) {
 	yahoo.historical({
@@ -8,4 +9,26 @@ exports.pullHistory = function(symbol, dates, callback) {
 	  to: dates.to,
 	  // period: 'd'  // 'd' (daily), 'w' (weekly), 'm' (monthly), 'v' (dividends only)
 	}, callback)
+}
+
+exports.findInflections = function(history, callback) {
+	var inflectionDays = [];
+	var lastClosingPrice = history[0].close;
+	var upwardTrend = history[1].close > lastClosingPrice;
+
+	history.forEach(function(val, index, array) {
+		if (upwardTrend && (val.close < lastClosingPrice)) {
+			inflectionDays.push(val);
+			upwardTrend = !upwardTrend;
+		}
+
+		if (!upwardTrend && (val.close > lastClosingPrice)) {
+			inflectionDays.push(val);
+			upwardTrend = !upwardTrend;
+		}
+
+		lastClosingPrice = val.close;
+	})
+
+	callback(null, inflectionDays)
 }
