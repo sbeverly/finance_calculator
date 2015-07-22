c159db1231c2e703a5567517688890f3c434c3e6
Siyan Beverly <siyan.beverly@gmail.com>
2015-07-22 19:37:45 -0400

    create server using test values

--- finance.js
+++ finance.js
@@ -1,4 +1,11 @@
 var http = require('http');
+var investmentCalculator = require('./investmentCalculator');
 
 http.createServer(function(req, res) {
+	investmentCalculator.buildReport('AAPL', {from: '2012-01-01', to: '2012-12-31'}, function(err, data) {
+		if (err) throw err;
+		res.writeHead(200, {'Access-Control-Allow-Origin': 'http://localhost:8000/'})
+		res.write(JSON.stringify(data));
+		res.end();
+	});
 }).listen(8000)
